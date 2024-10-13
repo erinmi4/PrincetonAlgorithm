dc9883d7c4812ababd63676723a14bb5f0274cd8
mika <mikasa2511592510@gmail.com>
2024-10-13 20:16:44 +0800

    初始状态

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,144 +1,51 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * 这个类用于模拟渗透（Percolation）问题.
- *
- * @param nmatrix        地图的大小，决定了模拟的范围。
- * @param size           已经被打开的个数，用于跟踪渗透状态。
- * @param percolationmap 地图本身，使用一个加权树来构成，用于表示渗透系统。
- * @param openSituation  用一个数组来表述所有的 site 的打开情况，方便查询和更新状态。
- * @param top            创建的顶层虚拟节点，可以与顶部的 nmatrix 个 site 直接连接，用于简化渗透判断。
- * @param bottom         创建的底部虚拟节点，可以与底部的 nmatrix 个 site 直接连接，同样用于简化渗透判断。
- * @author [Mika]
- * @version 1.0
- */
 public class Percolation {
+//按照约定，行索引和列索引是 1 和n之间的整数，其中 (1, 1) 是左上角站点
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
 
-  private int nmatrix;
-  private int size;
-  private WeightedQuickUnionUF percolationmap;
-  /*0是封闭的
-   * 1是打开了的*/
-  private int[] openSituation;
-  private int top;
-  private int bottom;
-
-  /**
-   * 构造函数，用于初始化渗透模型.
-   *
-   * @param nmatrix 网格的大小，构造成一个nmatrix x nmatrix的网格
-   */
-  public Percolation(int n) {
-    this.size = 0;
-    this.nmatrix = n;
-    percolationmap = new WeightedQuickUnionUF(nmatrix * nmatrix + 2);
-    openSituation = new int[nmatrix * nmatrix + 2];
-    top = nmatrix * nmatrix;
-    bottom = nmatrix * nmatrix + 1;
-    openSituation[top] = 1;
-    openSituation[bottom] = 1;
-
-    for (int i = 0; i < nmatrix; i++) {
-      percolationmap.union(top, i);
     }
-    for (int i = nmatrix * (nmatrix - 1); i < nmatrix * nmatrix; i++) {
-      percolationmap.union(bottom, i);
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
     }
-  }
 
-  /**
-   * 用来将一个坐标的site打开.
-   */
-  public void open(int row, int col) {
-    checkSites(row, col);
-    if (isOpen(row, col)) {
-      return;
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
     }
-    size++;
-    int location = xyTo1D(row, col);
-    openSituation[location] = 1;
 
-    List<Integer> neighbors = getValidNeighbors(row, col);
-    for (int neighbor : neighbors) {
-      if (openSituation[neighbor] == 1) {
-        percolationmap.union(location, neighbor);
-      }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+
     }
-  }
 
-  private int xyTo1D(int r, int c) {
-    return r * nmatrix + c;
-  }
+    // returns the number of open sites
+    public int numberOfOpenSites() {
 
-  public boolean isOpen(int row, int col) {
-    checkSites(row, col);
-    return openSituation[xyTo1D(row, col)] == 1;
-  }
-
-  /**
-   * 判断该点是否能够从顶部联通过来.
-   */
-  public boolean isFull(int row, int col) {
-    checkSites(row, col);
-    // 如果位置已打开并且与顶部连通，则该位置是 full
-    int location = xyTo1D(row, col);
-    if (isOpen(row, col) && percolationmap.connected(top, location)) {
-
-      return true;
     }
-    return false;
-  }
 
+    // does the system percolate?
+    public boolean percolates() {
 
-  public int numberOfOpenSites() {
-    return size;
-  }
+    }
+    //可以通过行列坐标，计算出该grid在整个图中的位置.
+    public int positionCalculate(int row, int col) {
 
-  public boolean percolates() {
-    return percolationmap.connected(top, bottom);
-  }
+    }
 
-  /**
-  *判断周围的点是否打开.
-  */
-  private int aroundppenhad(int row, int col) {
-    List<Integer> neighbors = getValidNeighbors(row, col);
-    for (int site : neighbors) {
-      if (openSituation[site] == 1) {
-        return site;
-      }
+    //判断参数是否符合范围,任何参数超出其范围，则抛出IllegalArgumentException
+    public void validPositon(int row, int col) {
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException("row or col is out of range");
+        }
     }
-    return -1;
-  }
 
-  /**
-   * 获取周围合法的点的坐标.
-   */
-  public List<Integer> getValidNeighbors(int row, int col) {
-    List<Integer> validSites = new ArrayList<>();
-    if (row > 0) {
-      validSites.add(xyTo1D(row - 1, col));
-    }
-    if (row < nmatrix - 1) {
-      validSites.add(xyTo1D(row + 1, col));
-    }
-    if (col > 0) {
-      validSites.add(xyTo1D(row, col - 1));
-    }
-    if (col < nmatrix - 1) {
-      validSites.add(xyTo1D(row, col + 1));
-    }
-    return validSites;
-  }
+    // test client (optional)
+    public static void main(String[] args) {
 
-  /**
-   * 判断坐标是否是合法坐标.
-   */
-  private void checkSites(int rows, int cols) {
-    if (rows < 0 || rows >= nmatrix || cols < 0 || cols >= nmatrix) {
-      throw new IllegalArgumentException();
     }
-  }
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,65 +1,33 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-
-/**
- * 配置需要的地图.
- */
 public class PercolationStats {
-  private final double mean;
-  private final double stddev;
-  private final double trials;
 
-  /**
-   * new.
-   */
-  public PercolationStats(int n, int trials) {
-    if (n <= 0 || trials <= 0) {
-      throw new IllegalArgumentException();
-    }
-    this.trials = trials;
-    double[] ratio = new double[trials];
-    for (int i = 0; i < trials; i += 1) {
-      Percolation p = new Percolation(n);
-      while (!p.percolates()) {
-        int randRow = StdRandom.uniform(n);
-        int randCol = StdRandom.uniform(n);
-        p.open(randRow, randCol);
-      }
-      ratio[i] = ((double) p.numberOfOpenSites()) / (n * n);
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
     }
 
-    this.mean = StdStats.mean(ratio);
-    this.stddev = StdStats.stddev(ratio);
-  }
+    // sample mean of percolation threshold
+    public double mean() {
 
-  public double mean() {
-    return mean;
-  }
+    }
 
-  public double stddev() {
-    return stddev;
-  }
+    // sample standard deviation of percolation threshold
+    public double stddev() {
 
-  public double confidenceLo() {
-    return mean - 1.96 * stddev / Math.sqrt(trials);
-  }
+    }
 
-  public double confidenceHi() {
-    return mean + 1.96 * stddev / Math.sqrt(trials);
-  }
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
 
-  /**
-  * main.
-  * */
-  public static void main(String[] args) {
-    int trials = Integer.parseInt(args[0]);
-    int gridSize = Integer.parseInt(args[1]);
-    PercolationStats ps = new PercolationStats(gridSize, trials);
-    //System.out.printf("Grid Size: %d x %d | Number of Trials: %d%n", gridSize, gridSize, trials);
-    System.out.printf("mean                    = %f%n", ps.mean());
-    System.out.printf("stddev                  = %f.%n",
-        ps.stddev());
-    System.out.printf("95%% confidence interval = [%f, %f].%n", ps.confidenceLo(),
-        ps.confidenceHi());
-  }
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+
+    }
+
+   // test client (see below)
+   public static void main(String[] args) {
+
+   }
+
 }
